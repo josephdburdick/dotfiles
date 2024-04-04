e993688134ba84fd039a877d72be496c70dfa9eb
Joseph Burdick <josephdburdick@gmail.com>
2024-04-03 21:46:43 -0400

    Update neovim config

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,8 +1,2 @@
-vim.loader.enable()
-if vim.g.vscode ~= nil then
-    return require('numToStr.vscode')
-end
-require('numToStr.settings')
-require('numToStr.autocmd')
-require('numToStr.plugins')
-require('numToStr.keybinds')
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
