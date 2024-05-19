6b34daca5a3b8775510ae4de75ee8ca0cc8b8e1a
Joseph Burdick <josephdburdick@gmail.com>
2024-05-19 14:43:48 -0400

    Add neovim themes; add powerlevel

--- neovim/.config/nvim/lua/plugins/theme.lua
+++ neovim/.config/nvim/lua/plugins/theme.lua
@@ -1,7 +1,20 @@
 -- themes
 return {
-  { "ellisonleao/gruvbox.nvim" },
-  { "tokyonight.nvim" },
+  { "catppuccin/nvim", name = "catppuccin", lazy = false, priority = 1000 },
+  { "ellisonleao/gruvbox.nvim", lazy = false, priority = 1000 },
+  { "tokyonight.nvim", lazy = false, priority = 1000 },
+  {
+    "projekt0n/github-nvim-theme",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("github-theme").setup({
+        -- ...
+      })
+
+      vim.cmd("colorscheme github_dark")
+    end,
+  },
   {
     -- Theme inspired by Atom
     "navarasu/onedark.nvim",
@@ -14,11 +27,12 @@ return {
       opts.toggle_style_key = "<leader>uI"
       require("onedark").setup(opts)
     end,
-  }, -- Configure Lazyvim to load theme
+  },
+  { "EdenEast/nightfox.nvim", lazy = false, priority = 1000 },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "onedark",
+      colorscheme = "nightfox",
     },
   },
 }
