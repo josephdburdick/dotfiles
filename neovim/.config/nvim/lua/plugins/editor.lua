0b44c5c40b704c52cb13c71172d696bbe0363f10
Joe <josephdburdick@gmail.com>
2024-07-22 04:42:40 -0400

    Add jq to Brewfile; add color highlighting

--- neovim/.config/nvim/lua/plugins/editor.lua
+++ neovim/.config/nvim/lua/plugins/editor.lua
@@ -6,26 +6,26 @@ return {
   },
 
   -- use AI to generate code
-  -- {
-  --   "dpayne/CodeGPT.nvim",
-  --   dependencies = {
-  --     "nvim-lua/plenary.nvim",
-  --     "MunifTanjim/nui.nvim",
-  --   },
-  --
-  --   config = function()
-  --     require("codegpt.config")
-  --   end,
-  -- },
-  --
-  -- -- Use AI to debug
-  -- {
-  --   "piersolenski/wtf.nvim",
-  --   dependencies = {
-  --     "MunifTanjim/nui.nvim",
-  --   },
-  -- },
-  --
+  {
+    "dpayne/CodeGPT.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+
+    config = function()
+      require("codegpt.config")
+    end,
+  },
+
+  -- Use AI to debug
+  {
+    "piersolenski/wtf.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+  },
+
   {
     "mg979/vim-visual-multi",
     lazy = false,
