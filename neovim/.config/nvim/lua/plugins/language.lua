0bbb3fb1b65e77e213bea2299fc157a4f28f35ff
Joe <josephdburdick@gmail.com>
2024-08-19 13:42:32 -0400

    Add macOS toggle functions

--- neovim/.config/nvim/lua/plugins/language.lua
+++ neovim/.config/nvim/lua/plugins/language.lua
@@ -1,16 +1,16 @@
 return {
   -- -- Replacement for tsserver lsp config
   -- -- https://github.com/pmizio/typescript-tools.nvim
-  {
-    "pmizio/typescript-tools.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    ft = {
-      "javascript",
-      "typescript",
-      "javascriptreact",
-      "typescriptreact",
-    },
-  },
+  -- {
+  --   "pmizio/typescript-tools.nvim",
+  --   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  --   ft = {
+  --     "javascript",
+  --     "typescript",
+  --     "javascriptreact",
+  --     "typescriptreact",
+  --   },
+  -- },
   --
   {
     "OlegGulevskyy/better-ts-errors.nvim",
@@ -22,29 +22,29 @@ return {
       -- },
     },
   },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "jose-elias-alvarez/typescript.nvim",
-      init = function()
-        require("lazyvim.util").lsp.on_attach(function(_, buffer)
-          -- stylua: ignore
-          vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
-          vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
-        end)
-      end,
-    },
-  },
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        -- Use a sub-list to run only the first available formatter
-        -- javascript = { { "prettier" } },
-        typescript = { { "prettier" } },
-        -- typescriptreact = { { "prettier" } },
-        css = { { "prettier" } },
-      },
-    },
-  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   dependencies = {
+  --     "jose-elias-alvarez/typescript.nvim",
+  --     init = function()
+  --       require("lazyvim.util").lsp.on_attach(function(_, buffer)
+  --         -- stylua: ignore
+  --         vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+  --         vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
+  --       end)
+  --     end,
+  --   },
+  -- },
+  -- {
+  --   "stevearc/conform.nvim",
+  --   opts = {
+  --     formatters_by_ft = {
+  --       -- Use a sub-list to run only the first available formatter
+  --       -- javascript = { { "prettier" } },
+  --       typescript = { { "prettier" } },
+  --       -- typescriptreact = { { "prettier" } },
+  --       css = { { "prettier" } },
+  --     },
+  --   },
+  -- },
 }
