b762fb0e553793830d98a99d58436173f5db8fd7
Joe Burdick <josephdburdick@gmail.com>
2024-12-05 21:32:22 -0500

    Replace gpt tool; customize fzf window size

---
+++ neovim/.config/nvim/lua/plugins/search.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "ibhagwan/fzf-lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("fzf-lua").setup({
+        winopts = {
+          height = 0.95,
+          width = 0.95,
+        },
+      })
+    end,
+  },
+}
