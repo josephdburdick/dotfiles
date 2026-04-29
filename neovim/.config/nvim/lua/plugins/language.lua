65e0c8a983f7b8f292629665f4bad71d305a9651
Joe <josephdburdick@gmail.com>
2026-04-28 23:06:45 -0400

    Apply biome as default formatter

--- neovim/.config/nvim/lua/plugins/language.lua
+++ neovim/.config/nvim/lua/plugins/language.lua
@@ -35,16 +35,18 @@ return {
   --     end,
   --   },
   -- },
-  -- {
-  --   "stevearc/conform.nvim",
-  --   opts = {
-  --     formatters_by_ft = {
-  --       -- Use a sub-list to run only the first available formatter
-  --       -- javascript = { { "prettier" } },
-  --       typescript = { { "prettier" } },
-  --       -- typescriptreact = { { "prettier" } },
-  --       css = { { "prettier" } },
-  --     },
-  --   },
-  -- },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        javascript = { "biome" },
+        javascriptreact = { "biome" },
+        typescript = { "biome" },
+        typescriptreact = { "biome" },
+        json = { "biome" },
+        jsonc = { "biome" },
+        css = { "biome" },
+      },
+    },
+  },
 }
